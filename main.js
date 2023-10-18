c968ede7835869d1d039e75b46fda6a3bad75f7a
Megan Cain <145371841+megancain@users.noreply.github.com>
2023-10-18 17:35:01 -0400

    Added graphics

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ class Calculator {
     this.currentOperandTextElement = currentOperandTextElement;
     this.clear();
   }
+
   clear() {
     this.currentOperand = "";
     this.previousOperand = "";
     this.operation = undefined;
   }
+
   delete() {
     this.currentOperand = this.currentOperand.toString().slice(0, -1);
   }
